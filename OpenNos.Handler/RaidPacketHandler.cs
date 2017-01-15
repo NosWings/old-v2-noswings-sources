ee7b9543a2d6889a2eb92678a313377a2ead097e
Blowa <blowaxd693@gmail.com>
2017-01-15 20:19:16 +0100

    [ADD] RaidPacketHandler

---
+++ OpenNos.Handler/RaidPacketHandler.cs
@@ -0,0 +1,40 @@
+﻿using OpenNos.GameObject;
+
+namespace OpenNos.Handler
+{
+    public class RaidPacketHandler
+    {
+        #region Members
+
+        private readonly ClientSession _session;
+
+        #endregion
+
+        #region Instantiation
+
+        public RaidPacketHandler(ClientSession session)
+        {
+            _session = session;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public ClientSession Session
+        {
+            get
+            {
+                return _session;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+       
+        #endregion
+
+    }
+}
